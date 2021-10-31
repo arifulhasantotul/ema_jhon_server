ed7d3c150ed6aae6b3f6923560c23f9a3839690a
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-11-01 00:38:05 +0600

    try

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ async function run() {
       // Add orders api
       app.post("/orders", async (req, res) => {
          const order = req.body;
+         order.createdAt = new Date();
          const result = await orderCollection.insertOne(order);
          console.log("order", order);
          res.json(result);
